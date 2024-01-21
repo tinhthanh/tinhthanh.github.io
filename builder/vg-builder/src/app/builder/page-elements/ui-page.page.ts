02cd0a824670910dfffa5f4d9d01bdd48480fa79
tinhthanh <k40cntt@gmail.com>
2024-01-21 17:19:14 +0700

    builder

--- builder/vg-builder/src/app/builder/page-elements/ui-page.page.ts
+++ builder/vg-builder/src/app/builder/page-elements/ui-page.page.ts
@@ -1,17 +1,20 @@
 import {ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
-import {Container} from "../element.ui";
+import {Container, IElementUi} from "../element.ui";
 import { KeyValuePipe, NgClass, NgTemplateOutlet } from "@angular/common";
 import { BuilderFactoryPage } from "../builder-factory/builder-factory.page";
 import { ElBase } from "src/app/builder/el-base";
-
+import {addIcons} from 'ionicons';
+import {IonIcon, IonLabel} from '@ionic/angular/standalone';
+import { createOutline }  from 'ionicons/icons';
 @Component({
     standalone: true,
     selector: 'app-ui-page',
     template: `
    @if(uiElement) {
-    <div [ngClass]="uiElement.classes" class="builder-mode">
-      <div class="label-builder-mode ">
-        {{ uiElement.label }}
+    <div  [ngClass]="uiElement.classes" class="builder-mode">
+      <div class="flex flex-row align-items-center gap-1 label-builder-mode ">
+        <ion-label> {{ uiElement.label }}</ion-label>
+        <ion-icon class="text-2xl cursor-pointer" (click)="selectEl($event,uiElement)" name="create-outline"></ion-icon>
       </div>
       @if(uiElement.children && Object.keys(uiElement.children).length > 0) {
             @for( item of uiElement.children | keyvalue:  orderOriginal; track item ) {
@@ -26,8 +29,9 @@ import { ElBase } from "src/app/builder/el-base";
       }
      </div>
   <ng-template #inner let-children="children">
-     <div class="label-builder-mode ">
-        {{ children.label }}
+     <div class="flex flex-row align-items-center gap-1 label-builder-mode  ">
+       <ion-label> {{ children.label }}</ion-label>
+       <ion-icon class="text-2xl cursor-pointer" (click)="selectEl($event,children)" name="create-outline"></ion-icon>
       </div>
     @if(children.children && Object.keys(children.children).length > 0) {
           @for( item of children.children | keyvalue:  orderOriginal; track item ) {
@@ -53,10 +57,21 @@ import { ElBase } from "src/app/builder/el-base";
     width: 100%;
    }
   `],
-    imports: [KeyValuePipe, NgClass, NgTemplateOutlet, BuilderFactoryPage]
+  imports: [KeyValuePipe, NgClass, NgTemplateOutlet, BuilderFactoryPage, IonIcon, IonLabel]
 })
 export class UiPagePage extends ElBase<Container> implements OnInit{
+// edit = output<IElementUi>
+constructor() {
+  super();
+  addIcons({'create-outline' :createOutline })
+}
    override ngOnInit(): void {
       super.ngOnInit();
+  }
+  selectEl($event: Event,uiElement: IElementUi)  {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log(uiElement)
+    // this.uiElement = uiElement;
   }
 }
