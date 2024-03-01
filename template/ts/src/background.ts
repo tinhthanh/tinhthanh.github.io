58d7b6acf97ba96b01b96680ec943024f1e6b062
tinhthanh <k40cntt@gmail.com>
2024-03-01 17:35:02 +0700

    update

--- template/ts/src/background.ts
+++ template/ts/src/background.ts
@@ -1,2 +1,4 @@
 import {localStorageV, StoreKey} from "./local-storage";
+import { StoreGrCrud } from "./store-gr.crud";
 window['localStorageV'] = localStorageV;
+window['vetgo'] = new StoreGrCrud<any>('AKfycbz2VEm9-dhQG6UlbpkwK2HjhXkvwHxQfMzQrsz4HO5QCGvpcxUefs-ytbG9WjtA34CDeA');
